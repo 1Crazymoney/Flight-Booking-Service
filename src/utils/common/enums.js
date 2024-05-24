3c12c3711fb84c0ca60c5b35f994c958fc44096f
Ashutosh Shukla <er.ashutoshcse@gmail.com>
2024-05-25 00:49:13 +0530

    Added booking model along with utils from flight service

---
+++ src/utils/common/enums.js
@@ -0,0 +1,17 @@
+const seatType = {
+    BUSINESS:'business',
+    ECONOMY: 'economy',
+    PREMIUM_ECONOMY: 'premium-economy',
+    FIRST_CLASS: 'first-class'
+}
+const bookingStatus = {
+    BOOKED: 'booked',
+    CANCELLED: 'cancelled',
+    INITIATED: 'initiated',
+    PENDING: 'pending'
+}
+
+module.exports = {
+    seatType,
+    bookingStatus
+}
